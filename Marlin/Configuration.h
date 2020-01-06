21193e51f2685963da131a071708b5a4328aea89
stockcube <35804595+stockcube@users.noreply.github.com>
2020-01-06 10:01:24 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -940,7 +940,7 @@
 #define NOZZLE_TO_PROBE_OFFSET { -45, 0, 0 }
 
 // Certain types of probes need to stay away from edges
-#define MIN_PROBE_EDGE 20
+#define MIN_PROBE_EDGE 10
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 4800
